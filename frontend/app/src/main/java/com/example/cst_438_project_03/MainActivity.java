de4ce40c1ed911bf42ce17b1ee57cc1c93184c50
edgar <edgarpm1820@gmail.com>
2021-11-03 20:54:46 -0700

    Added a retrofit test page as well as a button on the main page to take you to the test.

--- frontend/app/src/main/java/com/example/cst_438_project_03/MainActivity.java
+++ frontend/app/src/main/java/com/example/cst_438_project_03/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.cst_438_project_03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button testAPIButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        testAPIButton = (Button) findViewById(R.id.apiButton);
+
+        testAPIButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, apiTest.class);
+                startActivity(intent);
+            }
+        });
     }
 }
