9eff54836959fff9ee16a7d59ad3e8b85e664233
eperalta@csumb.edu <eperalta@csumb.edu>
2021-11-14 17:56:59 -0800

    made a verifyLogin function that checks username and password Also passed that info if correct to the next page.

--- frontend/app/src/main/java/com/example/cst_438_project_03/Login.java
+++ frontend/app/src/main/java/com/example/cst_438_project_03/Login.java
@@ -8,10 +8,23 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class Login extends AppCompatActivity implements View.OnClickListener {
     View btnLogin;
     EditText etUsername;
     EditText etPassword;
+
+    String[] tempUsernames = new String[]{
+            "Admin",
+            "Test",
+            "Test2"
+    };
+    String[] tempPasswords = new String[]{
+            "123",
+            "test",
+            "test2"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +37,20 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
     }
 
     public void onClick(View v){
-        Intent i = new Intent(this, UserViewProfileActivity.class);
-        startActivity(i);
+        verifyLogin(etUsername.getText().toString(), etPassword.getText().toString());
+    }
+
+    private void verifyLogin(String userName, String password){
+        for(int i = 0; i < tempUsernames.length; i++){
+            if(userName.equals(tempUsernames[i])){
+                if(password.equals((tempPasswords[i]))){
+                    Intent j = new Intent(this, UserViewProfileActivity.class);
+                    j.putExtra("userName", userName);
+                    j.putExtra("password", password);
+                    startActivity(j);
+                }
+            }
+        }
     }
 }
 
--- frontend/app/src/main/java/com/example/cst_438_project_03/UserViewProfileActivity.java
+++ frontend/app/src/main/java/com/example/cst_438_project_03/UserViewProfileActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class UserViewProfileActivity extends AppCompatActivity {
@@ -13,10 +14,20 @@ public class UserViewProfileActivity extends AppCompatActivity {
     Button userEditProfileBtn;
     Button cancelBtn3;
 
+    TextView description;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_view_profile);
+
+        String userName = getIntent().getStringExtra("userName").toString();
+        String password = getIntent().getStringExtra("password").toString();
+
+        description = (TextView) findViewById(R.id.textView6);
+        description.setText(userName);
+        description.append("\n");
+        description.append(password);
 
         userEditProfileBtn = (Button) findViewById(R.id.userEditProfileBtn);
         cancelBtn3 = (Button) findViewById(R.id.cancelBtn3);
