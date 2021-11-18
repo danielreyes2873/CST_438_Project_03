89257bdbf5eae0f74bd96522d78b7cff0c9d80bb
eperalta@csumb.edu <eperalta@csumb.edu>
2021-11-17 23:28:22 -0800

    Changed verifyLogin Function. Now only continues if the username and password from the database match

--- frontend/app/src/main/java/com/example/cst_438_project_03/Login.java
+++ frontend/app/src/main/java/com/example/cst_438_project_03/Login.java
@@ -4,27 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Login extends AppCompatActivity implements View.OnClickListener {
     View btnLogin;
     EditText etUsername;
     EditText etPassword;
-
-    String[] tempUsernames = new String[]{
-            "Admin",
-            "Test",
-            "Test2"
-    };
-    String[] tempPasswords = new String[]{
-            "123",
-            "test",
-            "test2"
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,23 +28,52 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
         btnLogin.setOnClickListener(this);
+
     }
 
     public void onClick(View v){
         verifyLogin(etUsername.getText().toString(), etPassword.getText().toString());
     }
 
+
     private void verifyLogin(String userName, String password){
-        for(int i = 0; i < tempUsernames.length; i++){
-            if(userName.equals(tempUsernames[i])){
-                if(password.equals((tempPasswords[i]))){
-                    Intent j = new Intent(this, UserViewProfileActivity.class);
-                    j.putExtra("userName", userName);
-                    j.putExtra("password", password);
-                    startActivity(j);
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://quiz-time438.herokuapp.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        QuizTimeApi quizTimeApi = retrofit.create(QuizTimeApi.class);
+
+        Call<List<Users>> call = quizTimeApi.getUsers();
+
+        call.enqueue(new Callback<List<Users>>() {
+            @Override
+            public void onResponse(Call<List<Users>> call, Response<List<Users>> response) {
+                if(!response.isSuccessful()){
+                    System.out.println("Code: " + response.code());
+                    return;
                 }
+
+                List<Users> users = response.body();
+                for(Users user: users){
+                    if(user.getUsername().equals(userName)){
+                        if(user.getPassword().equals(password)){
+                            Intent j = new Intent(Login.this, UserViewProfileActivity.class);
+                            j.putExtra("userName", userName);
+                            j.putExtra("password", password);
+                            startActivity(j);
+                        }
+                    }
+                }
+
+                //Add error message here
             }
-        }
+
+            @Override
+            public void onFailure(Call<List<Users>> call, Throwable t) {
+                System.out.println(t.getMessage());
+            }
+        });
     }
 }
 
